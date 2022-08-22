b39716ee606cbe8c204dcdc4760f27918920cd95
erbkaiser <6014923+erbkaiser@users.noreply.github.com>
2022-08-22 15:47:50 +0200

    Update hints for auto-Cyclius

--- fc_preferences.js
+++ fc_preferences.js
@@ -213,7 +213,7 @@ FrozenCookies.preferenceValues = {
         default: 11,
     },
     autoCyclius: {
-        hint: "Continually swap Cyclius throughout the day for max CpS bonus. Disables Auto Pantheon",
+        hint: "Continually swap Cyclius throughout the day for max CpS bonus. Works best if auto-Pantheon is set, with Cyclius in JADE slot",
         display: ["Auto-Cyclius OFF", "Auto-Cyclius in RUBY and JADE", "Auto-Cyclius in all slots"],
         default: 0,
     },
