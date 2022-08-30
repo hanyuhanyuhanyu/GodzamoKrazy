c0283d8ffc4ecadd1e6df720f675be7c2b837c3a
erbkaiser <6014923+erbkaiser@users.noreply.github.com>
2022-08-30 20:18:09 +0200

    Add a one click recommended settings option

--- fc_main.js
+++ fc_main.js
@@ -3411,6 +3411,89 @@ function autoCycliusAction() {
     }
 }
 
+function recommendedSettingsAction() {
+    if (FrozenCookies.recommendedSettings == 1) {
+        // clicking options
+        FrozenCookies.autoClick = 1;
+        FrozenCookies.cookieClickSpeed = 250;
+        FrozenCookies.autoFrenzy = 1;
+        FrozenCookies.frenzyClickSpeed = 1000;
+        FrozenCookies.autoGC = 1;
+        FrozenCookies.autoReindeer = 1;
+        FrozenCookies.autoFortune = 1;
+        // autobuy options
+        FrozenCookies.autoBuy = 1;
+        FrozenCookies.otherUpgrades = 1;
+        FrozenCookies.autoBulk = 100;
+        FrozenCookies.autoBlacklistOff = 0;
+        FrozenCookies.blacklist = 0;
+        FrozenCookies.mineLimit = 1;
+        FrozenCookies.mineMax = 500;
+        FrozenCookies.factoryLimit = 1;
+        FrozenCookies.factoryMax = 500;
+        FrozenCookies.pastemode = 0;
+        // other auto options
+        FrozenCookies.autoAscend = 0;
+        FrozenCookies.HCAscendAmount = 0;
+        FrozenCookies.autoWrinkler = 1;
+        FrozenCookies.shinyPop = 0;
+        FrozenCookies.autoSL = 2;
+        FrozenCookies.sugarBakingGuard = 1;
+        FrozenCookies.autoGS = 1;
+        FrozenCookies.autoGodzamok = 1;
+        FrozenCookies.autoBank = 1;
+        FrozenCookies.autoBroker = 1;
+        FrozenCookies.autoLoan = 1;
+        FrozenCookies.minLoanMult = 777;
+        // Pantheon options
+        FrozenCookies.autoWorshipToggle = 1;
+        FrozenCookies.autoWorship0 = 2; // Godzamok
+        FrozenCookies.autoWorship1 = 8; // Mokalsium
+        FrozenCookies.autoWorship2 = 6; // Muridal
+        FrozenCookies.autoCyclius = 0;
+        // Spell options
+        FrozenCookies.towerLimit = 1;
+        FrozenCookies.manaMax = 37;
+        FrozenCookies.autoSpell = 2;
+        FrozenCookies.minCpSMult = 7;
+        FrozenCookies.autoFTHOFCombo = 0;
+        FrozenCookies.auto100ConsistencyCombo = 0;
+        FrozenCookies.autoSugarFrenzy = 0;
+        FrozenCookies.autoSweet = 0;
+        //Dragon options
+        FrozenCookies.autoDragon = 1;
+        FrozenCookies.petDragon = 1;
+        FrozenCookies.autoDragonToggle = 1;
+        FrozenCookies.autoDragonAura1 = 3; // Elder Batallion
+        FrozenCookies.autoDragonAura2 = 15; // Radiant Appetite
+        FrozenCookies.autoDragonOrbs = 0;
+        FrozenCookies.cortexLimit = 0;
+        FrozenCookies.cortexMax = 200;
+        // Season options
+        FrozenCookies.defaultSeason = 1;
+        FrozenCookies.autoEaster = 1;
+        FrozenCookies.autoHalloween = 1;
+        //Bank options
+        FrozenCookies.holdSEBank = 0;
+        FrozenCookies.setHarvestBankPlant = 1;
+        FrozenCookies.setHarvestBankType = 3;
+        FrozenCookies.maxSpecials = 1;
+        // Other options
+        FrozenCookies.FCshortcuts = 1;
+        FrozenCookies.simulatedGCPercent = 1;
+        //Display options
+        FrozenCookies.showMissedCookies = 0;
+        FrozenCookies.numberDisplay = 1;
+        FrozenCookies.fancyui = 1;
+        FrozenCookies.logging = 1;
+        FrozenCookies.purchaseLog = 0;
+        FrozenCookies.fpsModifier = 2;
+        FrozenCookies.trackStats = 0;
+        logEvent("recommendedSettings", "Set all options to recommended values");
+        FrozenCookies.recommendedSettings = 0;
+    }
+}
+
 function generateProbabilities(upgradeMult, minBase, maxMult) {
     var cumProb = [];
     var remainingProbability = 1;
@@ -5642,6 +5725,11 @@ function FCStart() {
         FrozenCookies.autoCycliusBot = 0;
     }
 
+    if (FrozenCookies.recommendedSettingsBot) {
+        clearInterval(FrozenCookies.recommendedSettingsBot);
+        FrozenCookies.recommendedSettingsBot = 0;
+    }
+
     // Remove until timing issues are fixed
     //  if (FrozenCookies.goldenCookieBot) {
     //    clearInterval(FrozenCookies.goldenCookieBot);
@@ -5820,6 +5908,13 @@ function FCStart() {
     if (FrozenCookies.autoCyclius) {
         FrozenCookies.autoCycliusBot = setInterval(
             autoCycliusAction,
+            FrozenCookies.frequency
+        );
+    }
+
+    if (FrozenCookies.recommendedSettings) {
+        FrozenCookies.recommendedSettingsBot = setInterval(
+            recommendedSettingsAction,
             FrozenCookies.frequency
         );
     }
