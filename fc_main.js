60940b18020984604d4e20173fc6b23b96503e0a
erbkaiser <6014923+erbkaiser@users.noreply.github.com>
2022-09-11 19:58:38 +0200

    Partial revert of dragon aura code
    Until I have time to properly fix it

--- fc_main.js
+++ fc_main.js
@@ -2573,35 +2573,24 @@ function autoDragonAura0Action() {
         !Game.Has("A crumbly egg") ||
         Game.dragonLevel < 5 ||
         FrozenCookies.autoDragonAura0 == 0 ||
-        FrozenCookies.autoDragonToggle == 0 ||
-        Game.dragonAura == FrozenCookies.autoDragonAura0 ||
-        Game.dragonAura1 == FrozenCookies.autoDragonAura0
+        FrozenCookies.autoDragonToggle == 0
     ) {
         return;
     }
+
+    if (Game.dragonAura == FrozenCookies.autoDragonAura0) return;
 
     if (FrozenCookies.autoDragonAura0 == FrozenCookies.autoDragonAura1) {
         FrozenCookies.autoDragonAura1 = 0;
         logEvent("autoDragon", "Can't set both auras to the same one!");
+        return;
     }
 
-    if (
-        Game.dragonLevel > 25 &&
-        Game.dragonAura == FrozenCookies.autoDragonAura1 &&
-        Game.dragonAura1 != FrozenCookies.autoDragonAura0
-    ) {
-        Game.specialTab = "dragon";
-        Game.SetDragonAura(FrozenCookies.autoDragonAura0, 1);
-        Game.ConfirmPrompt();
-        logEvent("autoDragon", "Set first dragon aura");
-        return;
-    } else if (
-        Game.dragonLevel >= FrozenCookies.autoDragonAura0 + 4 &&
-        Game.dragonAura != FrozenCookies.autoDragonAura0
-    ) {
+    if (Game.dragonLevel >= FrozenCookies.autoDragonAura0 + 4) {
         Game.specialTab = "dragon";
         Game.SetDragonAura(FrozenCookies.autoDragonAura0, 0);
         Game.ConfirmPrompt();
+        Game.ToggleSpecialMenu();
         logEvent("autoDragon", "Set first dragon aura");
         return;
     }
@@ -2611,31 +2600,19 @@ function autoDragonAura1Action() {
     if (
         !Game.Has("A crumbly egg") ||
         Game.dragonLevel < 26 ||
-        FrozenCookies.autoDragonAura0 == 0 ||
         FrozenCookies.autoDragonAura1 == 0 ||
-        FrozenCookies.autoDragonToggle == 0 ||
-        Game.dragonAura == FrozenCookies.autoDragonAura1 ||
-        Game.dragonAura1 == FrozenCookies.autoDragonAura1
+        FrozenCookies.autoDragonToggle == 0
     ) {
         return;
     }
 
-    if (
-        Game.dragonAura1 == FrozenCookies.autoDragonAura0 &&
-        Game.dragonAura != FrozenCookies.autoDragonAura1
-    ) {
-        Game.specialTab = "dragon";
-        Game.SetDragonAura(FrozenCookies.autoDragonAura1, 0);
-        Game.ConfirmPrompt();
-        logEvent("autoDragon", "Set second dragon aura");
-        return;
-    } else if (
-        Game.dragonAura == FrozenCookies.autoDragonAura0 &&
-        Game.dragonAura1 != FrozenCookies.autoDragonAura1
-    ) {
+    if (Game.dragonAura1 == FrozenCookies.autoDragonAura1) return;
+
+    if (Game.dragonLevel == 26) {
         Game.specialTab = "dragon";
         Game.SetDragonAura(FrozenCookies.autoDragonAura1, 1);
         Game.ConfirmPrompt();
+        Game.ToggleSpecialMenu();
         logEvent("autoDragon", "Set second dragon aura");
         return;
     }
