4394021a01f578b720bfd18036381cc689e50769
hanyuTransfer <30584926+hanyuhanyuhanyu@users.noreply.github.com>
2023-01-19 22:31:59 +0900

    Update frozen_cookies.js

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -20,7 +20,7 @@ var scriptElement =
 var baseUrl =
     scriptElement !== null
         ? scriptElement.getAttribute("src").replace(/\/frozen_cookies\.js$/, "")
-        : "https://github.erbkaiser.com/FrozenCookies/";
+        : "https://hanyuhanyuhanyu.github.io/GodzamoKrazy/";
 var FrozenCookies = {
     baseUrl: baseUrl,
     branch: "Main-",
